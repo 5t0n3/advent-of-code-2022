d6e27ad2854f5e888ed717abc3a17a111900f2d6
Zane Othman <zane.othman@gmail.com>
2022-12-01 09:55:43 -0800

    Revise day 1 solutions to be more idiomatic (?)

--- Day01Calories.hs
+++ Day01Calories.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List (sort)
+import Data.List (sortBy)
 import Data.List.Split (splitOn)
 import Formatting
 
 main :: IO ()
 main = do
   input <- readFile "input/day01.txt"
-  -- splitOn [""] groups the calories by elf (it essentially splits on double newlines)
-  let caloriesList = map sum . map (map read) . splitOn [""] . lines $ input
+  let caloriesList = map (sum . map read . lines) . splitOn "\n\n" $ input
       maxCalories = maximum caloriesList -- expected output: 68292
-      top3 = sum . take 3 . reverse . sort $ caloriesList -- expected output: 203203
+      -- `sortBy (flip compare)` sorts the list in reverse (i.e. descending order)
+      top3Total = sum . take 3 . sortBy (flip compare) $ caloriesList -- expected output: 203203
   fprintLn ("Part 1 (top calorie amount): " % int) maxCalories
-  fprintLn ("Part 2 (top 3 elves' calories): " % int) top3
+  fprintLn ("Part 2 (top 3 elves' calories): " % int) top3Total
