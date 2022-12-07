04b5392d40a22b07736ba4bb28c1b15bbec6185e
Zane Othman <zane.othman@gmail.com>
2022-12-07 10:02:10 -0800

    slightly less cursed day 7 solution :)

--- Day07Space.hs
+++ Day07Space.hs
@@ -1,95 +1,87 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List (foldl', stripPrefix)
+import Data.HashMap.Strict ((!))
+import qualified Data.HashMap.Strict as M
+import Data.List (foldl', stripPrefix, uncons)
 import qualified Data.List.Split as S
-import Data.Map.Strict ((!))
-import qualified Data.Map.Strict as M
-import Data.Maybe (mapMaybe)
+import Data.Maybe (fromJust)
 import Formatting
 
-data Entry = Directory String | File String Int deriving (Show)
+-- directories are represented as a list of all their components to avoid name collisions
+data Entry = Directory [String] | File Int deriving (Show)
 
--- TODO: parentDirs is redundant lmao
-type ParentDirs = M.Map String String
-
-type Children = M.Map String [Entry]
+-- directory -> children mapping
+type Children = M.HashMap [String] [Entry]
 
 data State = State
-  { currentDir :: String,
-    parents :: ParentDirs,
+  { currentDir :: [String],
     children :: Children
   }
   deriving (Show)
 
-defaultState :: State
-defaultState =
+baseState :: State
+baseState =
   State
-    { currentDir = "/",
-      parents = M.empty,
+    { currentDir = ["/"],
       children = M.empty
     }
 
 main :: IO ()
 main = do
   input <- readFile "input/day07.txt"
-  -- ignore the initial "cd /"
-  -- note: duplicate folders </3
+  -- `tail` ignores the initial "cd /"
   let splitCommands = tail . map (splitNoBlanks "\n") . splitNoBlanks "$ " $ input
-      State {parents = par, children = chl} = evalCommands splitCommands defaultState
-      directorySizes = M.fromList . map (\dir -> (dir, directorySize chl dir)) $ M.keys chl
-      smallDirectoryTotal = M.foldl' (\sum size -> if size <= 100000 then size + sum else sum) 0 directorySizes -- Expected: 1501149
-      -- PART 2: min to free
-      totalUsedSpace = directorySizes ! "/"
+      -- PART 1
+      State {children} = evalCommands splitCommands baseState
+      directorySizes = M.map (directorySize children) children
+      smallDirectoryTotal = M.foldl' (+) 0 . M.filter (<= 100000) $ directorySizes -- Expected: 1501149
+      -- PART 2
+      totalUsedSpace = directorySizes ! ["/"]
       currentFreeSpace = 70000000 - totalUsedSpace
       minToFree = 30000000 - currentFreeSpace
-      minActualFree = minimum . filter (> minToFree) . M.elems $ directorySizes -- Expected: 10096985
-  fprintLn ("Part 1 (smol directory size sum): " % int) smallDirectoryTotal
-  fprintLn ("Part 2 (min to free while enough): " % int) minActualFree
+      minAdequateFree = minimum . M.elems . M.filter (>= minToFree) $ directorySizes -- Expected: 10096985
+  fprintLn ("Part 1 (size of small directories): " % int) smallDirectoryTotal
+  fprintLn ("Part 2 (minimum directory size to delete for enough free space): " % int) minAdequateFree
   where
     -- essentially splitOn with dropBlanks added
     splitNoBlanks = S.split . S.dropBlanks . S.dropDelims . S.onSublist
 
 evalCommands :: [[String]] -> State -> State
 evalCommands [] state = state
-evalCommands (cmd : rest) state
+evalCommands (current : rest) state@State {currentDir = cwd, children}
   | command == "cd" = evalCommands rest cdState
   | command == "ls" = evalCommands rest lsState
   | otherwise = error ("Unexpected command: " ++ command)
   where
-    (command : args) = S.splitOn " " (head cmd)
-    cmdResult = tail cmd
+    (fullCommand, result) = fromJust . uncons $ current
+    (command : args) = S.splitOn " " fullCommand
+    -- cd
     targetDir = case head args of
-      ".." -> (parents state) ! (currentDir state)
-      dir -> withParents (currentDir state) dir
-    lsOutput = parseLsOutput cmdResult (currentDir state)
-    newChildren = M.insert (currentDir state) lsOutput (children state)
+      ".." -> tail cwd
+      dir -> dir : cwd
+    cdState = state {currentDir = targetDir}
+    -- ls
+    lsOutput = parseLsOutput result cwd
+    newChildren = M.insert cwd lsOutput children
     lsState =
       state
-        { children = newChildren,
-          parents = updateParents lsOutput (currentDir state) (parents state)
+        { children = newChildren
         }
-    cdState = state {currentDir = targetDir}
 
-directorySize :: Children -> String -> Int
-directorySize childMap dirName = foldl' (flip ((+) . childSize)) 0 $ childMap ! dirName
+-- Recursively calculates the size of a directory from its children
+directorySize :: Children -> [Entry] -> Int
+directorySize childMap = foldl' (flip ((+) . childSize)) 0
   where
-    childSize (File _ size) = size
-    childSize (Directory name) = directorySize childMap name
+    childSize (File size) = size
+    childSize (Directory name) = directorySize childMap (childMap ! name)
 
-parseLsOutput :: [String] -> String -> [Entry]
+parseLsOutput :: [String] -> [String] -> [Entry]
 parseLsOutput [] _ = []
 parseLsOutput (current : rest) currentDir = currentEntry : parseLsOutput rest currentDir
   where
     currentEntry = case stripPrefix "dir " current of
-      Just dir -> Directory $ withParents currentDir dir
+      Just dir -> Directory $ dir : currentDir
       Nothing ->
-        let [size, name] = S.splitOn " " current
-         in File name (read size)
-
-updateParents :: [Entry] -> String -> ParentDirs -> ParentDirs
-updateParents ((Directory name) : rest) currentDir parents = updateParents rest currentDir (M.insert name currentDir parents)
-updateParents (_ : rest) currentDir parents = updateParents rest currentDir parents
-updateParents [] _ parents = parents
-
-withParents :: String -> String -> String
-withParents currentDir name = currentDir ++ name ++ "/"
+        let [size, _] = S.splitOn " " current
+         in File $ read size
